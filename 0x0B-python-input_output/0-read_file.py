4f23a09fa8389e9b38f65e031b135ba08c1105a7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-13 03:45:43 +0100

    Updated the first task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-""" A function that reads a text file (UTF-8) and prints it to the standard input output.
-It throws an error if the filename is not defined.
-"""
+""" A function that reads a text file (UTF-8) and prints it to the standard input output. It throws an error if the filename is not defined"""
 
 
 def read_file(filename=""):
-    """Takes an argument: filename.
-    Prints: the text file content.
+    """Takes an argument: filename
+    Prints the text file content
     """
+    if(not filename):
+        raise ValueError("Filename must be specified")
     with open(filename, 'r', encoding="utf8") as file:
         file_content = file.read()
         print(file_content)
