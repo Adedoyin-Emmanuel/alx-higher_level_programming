47998b9ca4b9ef6792981fc28f8d37d805378280
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-13 13:25:53 +0100

    Modified the first file function to return the total characters writte

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ def write_file(filename="", text=""):
     """This functions takes a filename and a text string to write to
     the specified file. It also returns the number of characters written.
     """
-    with open(filename, 'r+', encoding="utf-8") as file_opened:
-        file_opened.write(text)
-        return (len(text))
+    with open(filename, 'w', encoding="utf-8") as file_opened:
+        char_written = file_opened.write(text)
+        return (char_written)
