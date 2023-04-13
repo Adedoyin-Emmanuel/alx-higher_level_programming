559e0eec18c0a022fce37335d78e65038e5acb14
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-13 06:06:55 +0100

    Checked code using pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-A function that reads a text file (UTF-8)
-and prints it to the standard input output.
-It throws an error if the filename is not defined
-"""
+"""A function that reads a text file (UTF-8)"""
 
 
 def read_file(filename=""):
-    """Takes an argument: filename
-    Prints the text file content
+    """Takes an argument filename and prints the text file content
     """
     with open(filename, encoding="utf-8") as file:
         filecontent = file.read()
