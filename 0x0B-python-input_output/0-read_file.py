fd60b5368cef9f72d14a841805de1c7788a8229e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-13 06:21:59 +0100

    updated the first task file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Takes an argument filename and prints the text file content."""
     with open(filename, encoding="utf-8") as openedfile:
         filecontent = openedfile.read()
-        print(filecontent, end="")
+        print(filecontent, end="") # type: ignore
