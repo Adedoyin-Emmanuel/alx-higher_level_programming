ef191a4917003537059bd0795703bd4a54353c2b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-19 22:12:54 +0100

    Finished the second task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+"""A class MyList that inherits from list"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+    def print_sorted(self):
+        print(sorted(self))
