1688994df016afd112c089791021ed4c7b20a47c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-19 19:24:23 +0100

    Finished the first optional task and added the testing file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,13 +4,20 @@ after each line containing specific string.
 """
 
 
-def append_after(filename=""):
-    with open(filename, "r+", encoding="utf-8") as file_opened:
-        read_file = file_opened.read()
-        while read_file:
-            if(not read_file):
-                break
-            #get the characters
-            print(read_file)
-            
-append_after("tests/0-read_file.txt")
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a text after each lone containing 
+    specific string.
+    Arguments:
+    filename: (string): The name of the file.
+    search_string: (string): The string to be searched for in the file.
+    new_string: (string): The string to be inserted.
+    """
+    text_to_append = ''
+    with open(filename, encoding="utf-8") as file_opened:
+        for current_line in file_opened:
+            text_to_append += current_line
+            if (search_string in current_line):
+                text_to_append += new_string
+    #open the file and write the text to it.
+    with open(filename, "w", encoding="utf-8") as file_opended2:
+        file_opended2.write(text_to_append)
