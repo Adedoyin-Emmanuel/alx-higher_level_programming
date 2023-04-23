947f47c8dab710d8212f020c7d604fa3dbcb153c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-24 00:20:21 +0100

    Bug fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@ or otherwise False.
 """
 
 
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """A function that returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class
     or otherwise False.
