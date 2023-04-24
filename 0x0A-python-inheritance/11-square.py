e6a9e41b420de40177482ece1c2c4d7ebf6eeb9a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-24 20:23:34 +0100

    Added docstrings to the class methods

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,7 +17,11 @@ class Square(RectangleClass):
         super().__init__(size, size)
 
     def area(self):
+        """Returns the area of the square."""
         return (self.__size * self.__size)
 
     def __str__(self):
+        """Returns the string representation of the
+        rectangle description: [Rectangle] <width>/<height>
+        """
         return ("[Square] {:d} {:d}".format(self.__size, self.__size))
