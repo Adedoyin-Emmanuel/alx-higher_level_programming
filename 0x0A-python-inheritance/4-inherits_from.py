36e449ad806e7ec659abff31b7efe356a0a696ee
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-24 01:08:54 +0100

    Fixed the the pycodestlye error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,7 @@ instance of a class that inherited (directly or indirectly)
 from the specified class.
 """
 
+
 def inherits_from(obj, a_class):
     """A function that returns True if the object is an.
     instance of a class that inherited (directly or indirectly).
