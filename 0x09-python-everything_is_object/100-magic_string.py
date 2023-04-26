d33e914faf1794d2947c18532587de9e0f0a27e6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-26 11:21:34 +0100

    Updated the first optional task

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that returns s string "BestSchool" n times the number
+of the iteration.
+"""
+
+
+def magic_string(n):
+    """Print a function the number of times n iterates"""
+    for i in range(n):
+        print("BestSchool")        
