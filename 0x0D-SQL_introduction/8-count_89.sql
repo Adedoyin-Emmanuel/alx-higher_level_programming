e834379b073b7ac7241a93d46f05ace38d52e2c6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-18 22:46:11 +0100

    Updated the 9th task

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL DB
-SELECT * FROM first_table WHERE id = 89
+COUNT SELECT * FROM first_table WHERE id = 89
