f3fd47028bb9166961215daab7cb1dd021fc901e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-05-18 22:47:15 +0100

    Bug fix

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL DB
-COUNT SELECT * FROM first_table WHERE id = 89
+SELECT COUNT(id) * FROM first_table WHERE id = 89
