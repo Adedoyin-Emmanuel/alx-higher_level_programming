6d0af6df79c5ab839a17a84e863cf0ca9f28ca55
doyin <doyin@doyinHpLaptop.doyin>
2023-06-14 05:38:55 +0100

    Bug fix for the 6th task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const argumentLength = process.argv.length;
 const firstArgument = parseInt(process.argv[2]);
-if (argumentLength < 3) {
+
+if (isNaN(firstArgument)) {
   console.log('Not a number');
-} else if (argumentLength === 3) {
-  console.log(firstArgument);
+} else {
+  const number = parseInt(firstArgument);
+  console.log('My number: ' + number);
 }
