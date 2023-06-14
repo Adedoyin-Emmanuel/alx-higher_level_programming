0af1f87bb1d3b246c578d366ea1aefefcd19ce05
doyin <doyin@doyinHpLaptop.doyin>
2023-06-14 05:47:32 +0100

    Bug fix for the 5th task

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,17 @@
 #!/usr/bin/node
-const firstArgument = process.argv[3];
-const secondArgument = process.argv[4];
+let firstArgument = process.argv[3];
+let secondArgument = process.argv[4];
+const arg = process.argv;
+
+if (!arg[2]) {
+  firstArgument = 'undefined';
+  secondArgument = 'undefined';
+} else if (!arg[3]) {
+  firstArgument = arg[2];
+  secondArgument = 'undefined';
+} else {
+  firstArgument = arg[2];
+  secondArgument = arg[3];
+}
+
 console.log(firstArgument + ' is ' + secondArgument);
