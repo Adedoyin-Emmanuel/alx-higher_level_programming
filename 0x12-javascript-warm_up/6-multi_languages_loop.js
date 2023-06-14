857faa5c3fddb2cd5179dcef5ae398e9a7bf64f6
doyin <doyin@doyinHpLaptop.doyin>
2023-06-14 05:23:49 +0100

    Fixed semistandard errors

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const messagesArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(let i =0; i < messagesArray.length; i++) {
+for (let i = 0; i < messagesArray.length; i++) {
   console.log(messagesArray[i]);
 }
