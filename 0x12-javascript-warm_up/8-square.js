81adad221a7ebfb03cdf87b1f1a49d1c3e8090be
doyin <doyin@doyinHpLaptop.doyin>
2023-06-14 05:35:09 +0100

    Fixed semistandard errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const squareSize = process.argv[2];
-if(isNaN(Number(squareSize))) {
+if (isNaN(Number(squareSize))) {
   console.log('Missing size');
-}else{
-  for (let i = 0; i < squareSize; i++){
+} else {
+  for (let i = 0; i < squareSize; i++) {
     let row = '';
-    for (let j = 0; j < squareSize; j++){
+    for (let j = 0; j < squareSize; j++) {
       row += 'X';
     }
-    console.log(row)
+    console.log(row);
   }
 }
-
