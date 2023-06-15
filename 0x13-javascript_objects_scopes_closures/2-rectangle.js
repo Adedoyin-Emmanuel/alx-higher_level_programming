c7b4462697ae01ed360be0bb71057c4d060e585a
doyin <doyin@doyinHpLaptop.doyin>
2023-06-15 03:48:09 +0100

    Fixed the third task bug

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (!(parseInt(w) < 0 || parseInt(h) < 0)) {
+    if ((parseInt(w) > 0) && parseInt(h) > 0) {
       this.width = w;
       this.height = h;
     }
