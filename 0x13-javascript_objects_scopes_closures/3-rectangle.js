fd663bd6b7e2fa157c53cd83f5b3b437b0138a20
doyin <doyin@doyinHpLaptop.doyin>
2023-06-15 03:54:01 +0100

    Fixed the 4th task bug

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (
-      parseInt(w) === 0 ||
-      parseInt(h) === 0 ||
-      parseInt(w) < 0 ||
-      parseInt(h) < 0
-    ) {
-      return;
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
-    
-    this.print = () =>{
-        for (let i = 0; i < this.width; i++){
-          let row = '';
-          for (let j = 0; i < this.height;  j++){
-           row += 'X';
-          }
-          console.log(row);
+
+    this.print = () => {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'X';
         }
-    }
+        console.log(row);
+      }
+    };
   }
 }
 module.exports = Rectangle;
