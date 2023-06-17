01238acc28fa7a9fc5f972e6032f45e5a778af42
doyin <doyin@doyinHpLaptop.doyin>
2023-06-17 06:05:23 +0100

    Finished all tasks for this project

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const dataObject = require("./101-data").dict;
+const dataObject = require('./101-data').dict;
 const newDict = {};
 
 for (const key in dataObject) {
-  if (typeof newDict[dataObject[key]] === "undefined") {
+  if (typeof newDict[dataObject[key]] === 'undefined') {
     newDict[dataObject[key]] = [];
   }
   newDict[dataObject[key]].push(key);
