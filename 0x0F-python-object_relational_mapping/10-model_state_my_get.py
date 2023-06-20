30954a3cf4a2c3f61e6f53f6cee55af36d2519ca
doyin <doyin@doyinHpLaptop.doyin>
2023-06-20 05:21:41 +0100

    Updated the 11th task

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-A script that lists all State objects from the
-database hbtn_0e_6_usa
+A script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
 """
 
 
 from sys import argv
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 
@@ -17,8 +17,6 @@ if __name__ == '__main__':
     InstanceSession = sessionmaker(bind=dbEngine)
     session = InstanceSession()
 
-    states = session.query(State).filter(
-        State.name.contains('a')).order_by(State.id).all()
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
+    state = session.query(State).filter(State.name == argv[4]).first()
+    print('Not found' if not state else state.id)
     session.close()
