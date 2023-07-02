dafa868beab8197ae1e5864561cec17d71766180
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-02 23:58:01 +0100

    Fixed pycodestyle errors for 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,5 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(response.text), response.text))
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(response.text), response.text))
