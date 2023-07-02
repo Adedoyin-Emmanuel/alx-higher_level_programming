9ce24426191bc0f2ea28e5fe14a5bf7c6bf331a5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-02 23:59:49 +0100

    Fixed pycodestyle errors for 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,8 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    response = requests.get('https://api.github.com/user',
+                            auth=(argv[1], argv[2]))
     try:
         print(response.json().get('id'))
     except ValueError:
