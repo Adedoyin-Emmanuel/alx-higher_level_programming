624de27ae7fb473c2d26aab8bd5aa8413e8560a8
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-26 04:43:01 +0100

    Fixed semistandard errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const requestURL = process.argv[2];
 
 request.get(requestURL, (error, response) => {
@@ -11,7 +11,7 @@ request.get(requestURL, (error, response) => {
   results.forEach((result, index) => {
     if (
       result.characters.includes(
-        "https://swapi-api.alx-tools.com/api/people/18/"
+        'https://swapi-api.alx-tools.com/api/people/18/'
       )
     ) {
       counter++;
