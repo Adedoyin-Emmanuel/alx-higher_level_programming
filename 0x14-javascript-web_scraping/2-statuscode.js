76e60e8453c9f74c06deb97a3bff886a7b5c7cdf
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-26 04:03:21 +0100

    Fixed semistandard errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,7 @@
 const request = require('request');
 const requestURL = process?.argv[2];
 
-
-request.get(requestURL, (error, response) =>{
-    if(error) return console.log(error);
-    console.log(`code: ${response.statusCode}`);
+request.get(requestURL, (error, response) => {
+  if (error) return console.log(error);
+  console.log(`code: ${response.statusCode}`);
 });
