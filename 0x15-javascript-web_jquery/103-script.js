b1118c2bc55c22a7ab6a56cf5eed79233e61a2e1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-03 05:23:31 +0100

    Finished all the tasks

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,27 @@
+$(document).ready(($) => {
+  const apiUrl = "https://www.fourtonfish.com/hellosalut/hello/?";
+
+  $("#btn_translate").on("click", () => {
+    translate();
+  });
+  $("#language_code").on("focus", () => {
+    $(this).keydown((e) => {
+      //check if the keycode is enter which is 13
+      if (e.keyCode == 13) {
+        translate();
+      }
+    });
+  });
+});
+
+const translate = () => {
+  $.ajax({
+    url: `${apiUrl}${$.param({ lang: $("#language_code").val() })}`,
+    success: (result) => {
+      $("#hello").text(result.hello);
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+};
